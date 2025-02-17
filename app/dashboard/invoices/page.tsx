6cfeca42cbe400949d9ca9ee1b74dbfffda28d10
Edith Guaraca C <eguaraca@planerp.ec>
2025-02-17 00:34:32 -0500

    invoice

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,3 @@
-import React from 'react'
-
-export const InvoicesPage = () => {
-  return (
-    <div>invoices Page</div>
-  )
-}
-
-export default InvoicesPage;
+export default function InvoicePage() {
+    return <p>Invoice Page</p>;
+  }
